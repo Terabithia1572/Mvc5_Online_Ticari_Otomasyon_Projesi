83e6d2e61eb3dff6c59b375e427caa938d935810
Yunus İNAN <legendys65@gmail.com>
2021-11-02 14:03:25 +0300

    OtherClasses/Added

---
+++ Mvc5_Online_Ticari_Otomasyon_Projesi/Models/Sınıflar/Faturalar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc5_Online_Ticari_Otomasyon_Projesi.Models.Sınıflar
+{
+    public class Faturalar
+    {
+        public int FaturaID { get; set; }
+        public string FaturaSeriNo { get; set; }
+        public string FaturaSiraNo { get; set; }
+        public DateTime Tarih { get; set; }
+        public string VergiDairesi { get; set; }
+        public DateTime Saat { get; set; }
+        public string TeslimEden { get; set; }
+        public string TeslimAlan { get; set; }
+
+    }
+}
