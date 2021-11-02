8082369c1f2cc0825c22378387f7031c3bbec0d6
Yunus İNAN <legendys65@gmail.com>
2021-11-02 13:47:19 +0300

    Classed/Added

---
+++ Mvc5_Online_Ticari_Otomasyon_Projesi/Models/Sınıflar/Cariler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Mvc5_Online_Ticari_Otomasyon_Projesi.Models.Sınıflar
+{
+    public class Cariler
+    {
+        [Key]
+        public int CariID { get; set; }
+        public string CariAd { get; set; }
+        public string CariSoyad { get; set; }
+        public string CariSehir { get; set; }
+        public string CariMail { get; set; }
+    }
+}
---
+++ Mvc5_Online_Ticari_Otomasyon_Projesi/Models/Sınıflar/Urun.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Mvc5_Online_Ticari_Otomasyon_Projesi.Models.Sınıflar
+{
+    public class Urun
+    {
+        [Key]
+        public int UrunID { get; set; }
+        public string UrunAd { get; set; }
+        public string Marka { get; set; }
+        public short Stok { get; set; }
+        public decimal AlisFiyat { get; set; }
+        public decimal SatisFiyat { get; set; }
+        public bool Durum { get; set; }
+        public string UrunGorsel { get; set; }
+    }
+}
