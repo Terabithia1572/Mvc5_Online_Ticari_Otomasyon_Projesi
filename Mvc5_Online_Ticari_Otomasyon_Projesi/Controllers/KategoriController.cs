2b24e6b0bbd4750fcfcd22a806e9a8cc0c717942
Yunus İNAN <legendys65@gmail.com>
2021-11-05 16:25:22 +0300

    ToListMethodWithKategori/Added

--- Mvc5_Online_Ticari_Otomasyon_Projesi/Controllers/KategoriController.cs
+++ Mvc5_Online_Ticari_Otomasyon_Projesi/Controllers/KategoriController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Mvc5_Online_Ticari_Otomasyon_Projesi.Models.Sınıflar;
 
 namespace Mvc5_Online_Ticari_Otomasyon_Projesi.Controllers
 {
     public class KategoriController : Controller
     {
         // GET: Kategori
+        Context c = new Context();
         public ActionResult Index()
         {
-            return View();
+            var degerler = c.Kategoris.ToList();
+            return View(degerler);
         }
     }
 }
