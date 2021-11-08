5813335e3992199cb2cfa07e16cfad559aa89b0a
Yunus İNAN <legendys65@gmail.com>
2021-11-08 15:15:30 +0300

    ProductController+View/Added

---
+++ Mvc5_Online_Ticari_Otomasyon_Projesi/Controllers/UrunController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc5_Online_Ticari_Otomasyon_Projesi.Models.Sınıflar;
+
+namespace Mvc5_Online_Ticari_Otomasyon_Projesi.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var urunler = c.Uruns.ToList();
+            return View(urunler);
+        }
+    }
+}
