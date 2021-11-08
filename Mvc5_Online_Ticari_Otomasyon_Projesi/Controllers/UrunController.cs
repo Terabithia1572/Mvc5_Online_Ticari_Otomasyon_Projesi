f3afd88ae5acb91d1c6198a1a89dcaed25c1b3d5
Yunus İNAN <legendys65@gmail.com>
2021-11-08 16:40:20 +0300

    ProductGetAll+Updated/Added

--- Mvc5_Online_Ticari_Otomasyon_Projesi/Controllers/UrunController.cs
+++ Mvc5_Online_Ticari_Otomasyon_Projesi/Controllers/UrunController.cs
@@ -13,19 +13,64 @@ namespace Mvc5_Online_Ticari_Otomasyon_Projesi.Controllers
         Context c = new Context();
         public ActionResult Index()
         {
-            var urunler = c.Uruns.ToList();
+            var urunler = c.Uruns.Where(x=>x.Durum==true).ToList();
             return View(urunler);
         }
 
         [HttpGet]
         public ActionResult YeniUrun()
         {
+            List<SelectListItem> deger1 = (from x in c.Kategoris.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.KategoriAd,
+                                               Value = x.KategoriID.ToString()
+                                           }).ToList();
+
+            ViewBag.dgr1 = deger1;
             return View();
         }
         [HttpPost]
         public ActionResult YeniUrun(Urun p)
         {
             c.Uruns.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult UrunSil(int id)
+        {
+            var deger = c.Uruns.Find(id);
+            deger.Durum = false;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult UrunGetir(int id)
+        {
+            List<SelectListItem> deger1 = (from x in c.Kategoris.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.KategoriAd,
+                                               Value = x.KategoriID.ToString()
+                                           }).ToList();
+
+            ViewBag.dgr1 = deger1;
+            var urundeger = c.Uruns.Find(id);
+            return View("UrunGetir", urundeger);
+        }
+
+        public ActionResult UrunGuncelle(Urun p)
+        {
+            var urn = c.Uruns.Find(p.UrunID);
+            urn.AlisFiyat = p.AlisFiyat;
+            urn.Durum = p.Durum;
+            urn.KategoriID = p.KategoriID;
+            urn.Marka = p.Marka;
+            urn.SatisFiyat = p.SatisFiyat;
+            urn.Stok = p.Stok;
+            urn.UrunAd = p.UrunAd;
+            urn.UrunGorsel = p.UrunGorsel;
             c.SaveChanges();
             return RedirectToAction("Index");
         }
